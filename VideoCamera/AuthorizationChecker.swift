77f7054339fb4e6f28bb6485c98822ea7d30e612
jldavid <jldavid.mail@gmail.com>
2024-03-06 23:45:16 -0500

    Initial commit

---
+++ VideoCamera/AuthorizationChecker.swift
@@ -0,0 +1,35 @@
+import Foundation
+import AVFoundation
+
+struct AuthorizationChecker {
+    static func checkCaptureAuthorizationStatus() async -> Status {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized:
+            return .permitted
+            
+        case .notDetermined:
+            let isPermissionGranted = await AVCaptureDevice.requestAccess(for: .video)
+            if isPermissionGranted {
+                return .permitted
+            } else {
+                fallthrough
+            }
+            
+        case .denied:
+            fallthrough
+            
+        case .restricted:
+            fallthrough
+            
+        @unknown default:
+            return .notPermitted
+        }
+    }
+}
+
+extension AuthorizationChecker {
+    enum Status {
+        case permitted
+        case notPermitted
+    }
+}
---
+++ VideoCamera/VideoCameraApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct VideoCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VideoContentView()
+        }
+    }
+}
